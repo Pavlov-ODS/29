cb268c03bf996ee4ab1552afe531f3540f8c6d74
Sergii Pavlov <4915906430009s@gmail.com>
2023-04-22 23:03:41 +0200

    2

--- script.js
+++ script.js
@@ -9,3 +9,19 @@ let nameSix = "Jak";
 let nameSeven = "Alex";
 let nameEight = "Jon";
 let nameNine= "Lola";*/
+
+// можно закоментировать 2 спрособомами.это первый
+/* это второй способ */
+
+/* написать переменніе можно так. 
+-var one=0;
+-let one=0;
+-let nomberOne=0
+const One = 0 */
+
+
+
+
+const currentYear = new Date().getFullYear();
+const age = currentYear - prompt("Введите год своего рождения:");
+alert(`Вам ${age} лет.`);
